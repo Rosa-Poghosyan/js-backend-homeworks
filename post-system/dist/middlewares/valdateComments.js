fb7db0435ed2b3a61fff3bd79d00c20347b4644f
Roza.Poghosyan <roza.poghosyan@helpsystems.com>
2025-09-08 00:50:30 +0400

    Remove dist folder

--- post-system/dist/middlewares/valdateComments.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateComment = validateComment;
-function validateComment(req, res, next) {
-    if (!req.body.commentData || req.body.commentData.trim() === "") {
-        return res.status(400).json({ message: "Comment cannot be empty" });
-    }
-    next();
-}
--- post-system/dist/middlewares/validatePosts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validatePost = validatePost;
-function validatePost(req, res, next) {
-    if (!req.body.postData || req.body.postData.trim() === "") {
-        return res.status(400).json({ message: "Post cannot be empty" });
-    }
-    next();
-}
